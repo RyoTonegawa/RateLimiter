a0bb82496d56ce2717aee4b9c5d62ab5d8492b35
RyoTonegawa <tonegawa622@gmail.com>
2026-05-12 02:19:50 +0900

    docs: sliding window counterのコメント追加

--- internal/limiter/sliding_window_counter.go
+++ internal/limiter/sliding_window_counter.go
@@ -33,24 +33,31 @@ func (l *SlidingWindowCounterLimiter) Allow(key string) bool {
 	defer l.mu.Unlock()
 
 	now := l.now()
+	// 現在自国が属しているWindowの開始時刻を取得
+	// 時刻をWindowの単位で切り捨て取得
 	windowStart := now.Truncate(l.window)
 	state, ok := l.counters[key]
 	if !ok {
 		state = &slidingCounterState{currentWindowStart: windowStart}
 		l.counters[key] = state
 	}
-
+	//currentとして見ているWindowの後の場合
 	if windowStart.After(state.currentWindowStart) {
+		//どのくらい進んだかを判断する。
+		// ちょうどwindowだけか、windowよりも長く進んでいるか
 		if windowStart.Sub(state.currentWindowStart) == l.window {
+			// ちょうどwindowだけ進んだ場合、currentだったものをpreviousとおく
 			state.previousCount = state.currentCount
 		} else {
+			// もはや前のwindowのカウントは関係ないのでゼロにリセット
 			state.previousCount = 0
 		}
 		state.currentCount = 0
 		state.currentWindowStart = windowStart
 	}
-
+	// ここから経過時間に合わせてwindowに重み付けしてpreviousのうちのこすカウントを算出する
 	elapsed := now.Sub(state.currentWindowStart)
+	// 前のwindowがどのくらい残っているか？をカウントに掛け、その分をcurrentと足してCountとする
 	previousWeight := float64(l.window-elapsed) / float64(l.window)
 	estimatedCount := float64(state.currentCount) + float64(state.previousCount)*previousWeight
 
